d979a320029381b193d29286f0f31cfe906f716c
mohamed ahmed hassan <mohamedahmedhassan@mohameds-Mac-mini.local>
2026-08-31 10:44:03 +0300

    feat(ِenrollment): finshed  enroll

---
+++ src/modules/enrollments/index.ts
@@ -0,0 +1,1 @@
+export { default as enrollmentRouter } from './routes/enrollment.routes.js';
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,21 +1,23 @@
-    import { Router } from 'express';
-    import { authRouter } from '../modules/auth/index.js';
-    import { sessionRouter } from '../modules/auth/index.js';
-    import { userRouter } from '../modules/users/index.js';
-    import { courseRouter } from '../modules/courses/index.js';
-    import { lessonRouter } from '../modules/lessons/index.js';
+import { Router } from 'express';
+import { authRouter } from '../modules/auth/index.js';
+import { sessionRouter } from '../modules/auth/index.js';
+import { userRouter } from '../modules/users/index.js';
+import { courseRouter } from '../modules/courses/index.js';
+import { lessonRouter } from '../modules/lessons/index.js';
+import { enrollmentRouter } from '../modules/enrollments/index.js';
 
 
-    const apiRouter = Router();
+const apiRouter = Router();
 
-    apiRouter.use('/auth',authRouter);
-    apiRouter.use('/sessions',sessionRouter);
+apiRouter.use('/auth', authRouter);
+apiRouter.use('/sessions', sessionRouter);
 
-    apiRouter.use('/users',userRouter);
+apiRouter.use('/users', userRouter);
 
-    apiRouter.use('/courses',courseRouter);
+apiRouter.use('/courses', courseRouter);
 
-    apiRouter.use('/', lessonRouter);
+apiRouter.use('/', lessonRouter);
+apiRouter.use('/',enrollmentRouter);
 
 
-    export default apiRouter;
+export default apiRouter;
